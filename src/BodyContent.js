551332ac86fa584aa5ac87084d058c63b0a14008
RoaaBS <s11924302@stu.najah.edu>
2024-09-04 20:35:35 +0300

    add spinner&img hover&i hover

--- src/BodyContent.js
+++ src/BodyContent.js
@@ -1,24 +1,37 @@
-// BodyContent.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Spinner from './Spinner';
 import Navbar from './Navbar';
 import LeftSidebar from './LeftSidebar';
 import RightSidebar from './RightSidebar';
 import ContentBody from './ContentBody';
-import './BodyContent.css'; // Ensure you have the corresponding CSS file
+import './BodyContent.css';
 
 const BodyContent = () => {
-    return (
-        <div className="body-content">
-            
-            <Navbar />
-            <ContentBody/>
-            <div className="content">
-                <LeftSidebar />
-                <RightSidebar />
-            </div>
-           
-        </div>
-    );
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate a data fetch or loading
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000); // Adjust time as needed
+  }, []);
+
+  return (
+    <div className="body-content">
+      {loading ? (
+        <Spinner />
+      ) : (
+        <>
+          <Navbar />
+          <ContentBody />
+          <div className="content">
+            <LeftSidebar />
+            <RightSidebar />
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default BodyContent;
---
+++ src/Spinner.js
@@ -0,0 +1,13 @@
+// Spinner.js
+import React from 'react';
+import './Spinner.css'; // Ensure you have the corresponding CSS file
+
+const Spinner = () => {
+  return (
+    <div className="spinner-container">
+      <div className="spinner"></div>
+    </div>
+  );
+};
+
+export default Spinner;
