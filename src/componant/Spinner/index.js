ed7074e5f1e8d67250e396a44ffa153765236032
RoaaBS <113924654+RoaaBS@users.noreply.github.com>
2024-09-05 20:21:45 +0300

    refactor(components): organize into modules

--- src/componant/Spinner/index.js
+++ src/componant/Spinner/index.js
@@ -1,12 +1,6 @@
 // Spinner.js
 import React from 'react';
-<<<<<<< HEAD:src/componant/Spinner/index.js
 import './index.css'; 
-=======
-import './Spinner.css'; 
-
->>>>>>> origin/Ticket#1:src/Spinner.js
-
 const Spinner = () => {
   return (
     <div className="spinner-container">
